68d28169bab5f6933795b9b9bf98b20c2bb1582c
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-01 20:39:37 -0400

    Add Python solution for String Construction

---
+++ algorithms/strings/string_construction/string_construction.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+"""Solution for https://www.hackerrank.com/challenges/string-construction"""
+
+
+def calc_min_cost(string: str) -> int:
+    """Determine the minimum cost of a string."""
+    cost = 0
+
+    if not string:
+        return 0
+
+    characters = set()
+
+    for char in string:
+        if char not in characters:
+            cost += 1
+            characters.add(char)
+
+    return cost
+
+
+def main():
+    """Print the minimum cost of each string."""
+    string_count = int(input().strip())
+    for _ in range(string_count):
+        string = input().strip()
+        result = calc_min_cost(string)
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
