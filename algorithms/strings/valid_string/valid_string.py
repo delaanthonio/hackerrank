4ef7f03dfa1a3651020ae4c11004d03d245de2bb
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-01 21:34:32 -0400

    Add Python solution for Sherlock and the Valid String

---
+++ algorithms/strings/valid_string/valid_string.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+"""Solution for https://www.hackerrank.com/challenges/sherlock-and-valid-string"""
+
+from collections import defaultdict
+
+
+def is_valid_string(string: str) -> bool:
+    """Determine if a string is valid."""
+
+    if not string:
+        return False
+
+    characters = defaultdict(int)
+
+    for char in string:
+        characters[char] += 1
+
+    counts = defaultdict(int)
+
+    for value in characters.values():
+        counts[value] += 1
+
+    if len(counts) == 1:
+        return True
+
+    if len(counts) == 2:
+        return 1 in counts.values()
+
+    return False
+
+
+def main():
+    """Determine if strings are valid."""
+    string = input().strip()
+    result = is_valid_string(string)
+    if result:
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == '__main__':
+    main()
