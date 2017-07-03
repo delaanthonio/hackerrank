2502b945dc2a9b4fb48f351eb78e85b18267dddf
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-03 13:30:18 -0400

    Add Go solution for Solve Me First

---
+++ algorithms/warmup/solve_me_first.go
@@ -0,0 +1,16 @@
+/* Solution for https://www.hackerrank.com/challenges/solve-me-first */
+
+package main
+
+import "fmt"
+
+func solveMeFirst(a uint32, b uint32) uint32 {
+	return a + b
+}
+
+func main() {
+	var a, b, res uint32
+	fmt.Scanf("%v\n%v", &a, &b)
+	res = solveMeFirst(a, b)
+	fmt.Println(res)
+}
