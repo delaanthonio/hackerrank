05035ab4b8f43bb1c95b2bbd51d5162fe97abfa3
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-17 18:05:06 -0400

    templates: Add Python solution template

---
+++ templates/template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Solution for: """
+
+
+def helper_fun() -> None:
+    """Calculate ..."""
+    pass
+
+
+def main():
+    """Print ..."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
