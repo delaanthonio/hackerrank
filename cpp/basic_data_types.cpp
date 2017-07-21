7a93c6809697e944e2d2a9a808cd5e55fb371fed
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-21 13:51:29 -0400

    cpp: Add basic_data_types

---
+++ cpp/basic_data_types.cpp
@@ -0,0 +1,17 @@
+/* https://www.hackerrank.com/challenges/c-tutorial-basic-data-types */
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d\n%ld\n%c\n%.3f\n%lf\n", i, l, c, f, d);
+    return 0;
+}
