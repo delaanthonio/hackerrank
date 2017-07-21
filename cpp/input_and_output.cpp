06c94ecf66c5dd91cdec1f9064c0bccf2168e868
Dela Anthonio <dell.anthonio@gmail.com>
2017-07-21 13:41:31 -0400

    cpp: Add input_and_output

---
+++ cpp/input_and_output.cpp
@@ -0,0 +1,16 @@
+/* https://www.hackerrank.com/challenges/cpp-input-and-output */
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
