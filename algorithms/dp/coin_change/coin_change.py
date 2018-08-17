01fb05add3f0856015938157ed94f523f7314738
Dela Anthonio <dell.anthonio@gmail.com>
2018-08-17 10:40:10 -0400

    refactor(python): Simplify solution to the coin change problem.

--- algorithms/dp/coin_change/coin_change.py
+++ algorithms/dp/coin_change/coin_change.py
@@ -6,27 +6,13 @@ Solution for https://www.hackerrank.com/challenges/coin-change/problem
 from typing import List
 
 
-def count_ways(target: int, coins: List[int]) -> int:
-    """
-    Count the number of ways we can count to ``target`` with values ``coins``.
-
-    :param target: The target value.
-    :param coins: The coins we can use to reach target.
-    :returns: The amount of ways we can count to target using coins.
-    """
-    cols = target + 1
-    rows = len(coins) + 1
-    table = [[0] * cols for _ in range(rows)]
-    for row in range(1, rows):
-        coin = coins[row - 1]
-        for col in range(1, cols):
-            if coin > col:
-                table[row][col] = table[row - 1][col]
-            elif coin == col:
-                table[row][col] = table[row - 1][col] + 1
-            else:
-                table[row][col] = table[row - 1][col] + table[row][col - coin]
-    return table[-1][-1]
+def count_ways(amount: int, coins: List[int]) -> int:
+    """Return the number of ways we can count to ``amount`` with values ``coins``."""
+    ways = [1] + [0] * amount
+    for coin in coins:
+        for val in range(coin, amount + 1):
+            ways[val] += ways[val - coin]
+    return ways[-1]
 
 
 def main():
