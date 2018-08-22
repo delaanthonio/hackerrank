26360672b18ac5ae193892438200a8c3a07a3e32
Dela Anthonio <dell.anthonio@gmail.com>
2018-08-22 19:16:18 -0400

    solution(cpp): The Coin Change Problem

---
+++ algorithms/dp/coin_change/coin_change.cpp
@@ -0,0 +1,35 @@
+/*
+ * problem:https://www.hackerrank.com/challenges/coin-change/problem
+ */
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long coin_change(int amount, int size, int coins[]) {
+    long long *ways = (long long *)calloc(amount + 1, sizeof(*ways));
+    ways[0] = 1;
+    for (int i = 0; i < size; i++) {
+        int coin = coins[i];
+        for (int j = coin; j <= amount; j++) {
+            ways[j] += ways[j - coin];
+        }
+    }
+    free(ways);
+    return ways[amount];
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    int coins[n];
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+    long long ways = coin_change(m, n, coins);
+    cout << ways << endl;
+    return 0;
+}
