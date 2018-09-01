2106fb85d4b9200c75f3215aee02b06461d7d5ff
Dela Anthonio <dell.anthonio@gmail.com>
2018-09-01 09:27:43 -0400

    solution(python): Two Strings

---
+++ algorithms/strings/two_strings/two_strings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+:problem: https://www.hackerrank.com/challenges/two-strings/problem
+"""
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        s1 = set(char for char in input())
+        s2 = set(char for char in input())
+        if s1.isdisjoint(s2):
+            print("NO")
+        else:
+            print("YES")
+
+
+if __name__ == '__main__':
+    main()
