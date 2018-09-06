9ae00933ddc3787f6a944aa38de070f1b797e498
Dela Anthonio <dell.anthonio@gmail.com>
2018-09-06 10:52:01 -0400

    [java] Welcome to Java

---
+++ lang/java/intro/welcome_to_java/solution.java
@@ -0,0 +1,14 @@
+/**
+ * Welcome to Java!
+ *
+ * @author Dela Anthonio
+ * @hackerrank https://hackerrank.com/delaanthonio
+ * @problem https://hackerrank.com/challenges/welcome-to-java
+ */
+
+public class solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
