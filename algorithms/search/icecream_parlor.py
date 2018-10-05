5cc83a305286d9424ad6ab7d91b3c32ebe260e4e
Dela Anthonio <dell.anthonio@gmail.com>
2018-10-05 07:46:28 -0400

    [python(refactor)] Ice Cream Parlor

--- algorithms/search/icecream_parlor.py
+++ algorithms/search/icecream_parlor.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
-"""Problem: https://www.hackerrank.com/challenges/icecream-parlor/problem"""
+"""
+Ice Cream Parlor
+
+:author: Dela Anthonio
+:hackerrank: https://hackerrank.com/delaanthonio
+:problem: https://www.hackerrank.com/challenges/icecream-parlor
+"""
+
+from typing import List, Tuple
+
+
+def find_indicies(money: int, prices: List[int]) -> Tuple[int, int]:
+    price_indices = {}
+    for i, price in enumerate(prices, start=1):
+        if money - price in price_indices:
+            return price_indices[money - price], i
+        price_indices[price] = i
+    return 0, 0
 
 
 def main():
     trips = int(input())
     for _ in range(trips):
-        dollars = int(input())
+        money = int(input())
         flavors = int(input())
         prices = [int(x) for x in input().split()]
-        price_table = {}
-        for i, price in enumerate(prices, start=1):
-            if dollars - price in price_table:
-                print(*sorted([i, price_table[dollars - price]]))
-                break
-            price_table[price] = i
+        indices = find_indicies(money, prices)
+        print(*indices)
 
 
 if __name__ == '__main__':
