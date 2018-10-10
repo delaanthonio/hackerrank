7b5a87bc9a32a622940844021655c984d2fabe11
Dela Anthonio <dell.anthonio@gmail.com>
2018-10-10 10:04:25 -0400

    [python] Hello World

---
+++ lang/python/intro/hello_world/solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Say "Hello, World!" With Python
+
+:author: Dela Anthonio
+:hackerrank: https://hackerrank.com/delaanthonio
+:problem: https://www.hackerrank.com/challenges/py-hello-world
+"""
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == '__main__':
+    main()
