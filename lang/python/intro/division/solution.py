b405842d95c86dc6c51bcdd396f355dabe8c5ae8
Dela Anthonio <dell.anthonio@gmail.com>
2018-10-10 13:03:41 -0400

    [python] Division

---
+++ lang/python/intro/division/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Python: Division
+
+:author: Dela Anthonio
+:hackerrank: https://hackerrank.com/delaanthonio
+:problem: https://www.hackerrank.com/challenges/python-division
+"""
+
+
+def main():
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
+
+
+if __name__ == '__main__':
+    main()
