2d35610b872b6b918b0e246f49c1afefb27662a7
Dela Anthonio <dell.anthonio@gmail.com>
2018-10-22 11:16:59 -0400

    [python] Arithmetic Operators

---
+++ lang/python/intro/arithmetic_operators/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Arithmetic Operators
+
+:author: Dela Anthonio
+:hackerrank: https://hackerrank.com/delaanthonio
+:problem: https://www.hackerrank.com/challenges/python-arithmetic-operators
+"""
+
+
+def main():
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+
+if __name__ == '__main__':
+    main()
