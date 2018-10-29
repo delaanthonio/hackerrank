b23db1da704bee7ec8142807ff1bdc1429dd4baa
Dela Anthonio <dell.anthonio@gmail.com>
2018-10-29 17:49:51 -0400

    [java][template] Add template for Java

---
+++ templates/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ *
+ *
+ * @author Dela Anthonio
+ * @hackerrank https://hackerrank.com/delaanthonio
+ * @problem
+ */
+public class solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		System.out.println();
+		scan.close();
+	}
+}
