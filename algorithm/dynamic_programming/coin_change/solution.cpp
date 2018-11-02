8ff87085dccc7f6be2e40ddab1cfcc6f911529de
Dela Anthonio <dell.anthonio@gmail.com>
2018-11-02 17:47:05 -0400

    [c++][refactor] The Coin Change Problem

--- algorithm/dynamic_programming/coin_change/solution.cpp
+++ algorithm/dynamic_programming/coin_change/solution.cpp
@@ -2,36 +2,38 @@
 //
 // author: Dela Anthonio
 // hackerrank: https://hackerrank.com/delaanthonio
-// problem: https://www.hackerrank.com/challenges/coin-change/problem
+// problem: https://www.hackerrank.com/challenges/coin-change
 
 #include <algorithm>
 #include <cmath>
-#include <cstdio>
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-long long coin_change(int amount, int size, int coins[]) {
-    long long *ways = (long long *)calloc(amount + 1, sizeof(*ways));
+long long coin_change(int amount, vector<int> coins) {
+    vector<long long> ways(amount + 1);
     ways[0] = 1;
-    for (int i = 0; i < size; i++) {
-        int coin = coins[i];
-        for (int j = coin; j <= amount; j++) {
-            ways[j] += ways[j - coin];
+
+    for (auto coin = begin(coins); coin != end(coins); coin++) {
+        for (int j = *coin; j <= amount; j++) {
+            ways[j] += ways[j - *coin];
         }
     }
-    free(ways);
-    return ways[amount];
+
+    return ways.back();
 }
 
 int main() {
     int m, n;
     cin >> m >> n;
-    int coins[n];
+    vector<int> coins(n);
+
     for (int i = 0; i < n; i++) {
         cin >> coins[i];
     }
-    long long ways = coin_change(m, n, coins);
+
+    long long ways = coin_change(m, coins);
     cout << ways << endl;
     return 0;
 }
