10b4762703c06b5bf403d291404b753fa08c7410
Dela Anthonio <dell.anthonio@gmail.com>
2019-01-28 10:46:46 -0500

    [java] Update Template

--- template/solution.java
+++ template/solution.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
  */
 public class solution {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
 
 		System.out.println();
-		scan.close();
+		scanner.close();
 	}
 }
