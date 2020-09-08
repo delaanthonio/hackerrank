ea0415aa5c63af71a8ca325fbb91f12ad9f59f3d
Dela Anthonio <dela.anthonio@pm.me>
2020-09-07 20:46:37 -0400

    [c++][python] Forming a magic square

---
+++ algorithm/implementation/forming_a_magic_square/forming_a_magic_square.cpp
@@ -0,0 +1,22 @@
+// Forming a Magic Square
+//
+// author: Dela Anthonio
+// hackerrank: https://hackerrank.com/delaanthonio
+// problem: https://www.hackerrank.com/challenges/
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    return 0;
+}
---
+++ algorithm/implementation/forming_a_magic_square/forming_a_magic_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+
+
+:author: Dela Anthonio
+:hackerrank: https://hackerrank.com/delaanthonio
+:problem: https://www.hackerrank.com/challenges/
+"""
+
+
+def func() -> int:
+    pass
+
+
+def main():
+    n = int(input())
+
+
+if __name__ == '__main__':
+    main()
